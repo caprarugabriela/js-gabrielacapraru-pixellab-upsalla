ef0f591b3bf20621c58a3f24a33340fa110f3c21
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2023-09-12 19:14:40 +0300

    Tema_WIP_5.09

--- m01s02/ex01/js/utils.js
+++ m01s02/ex01/js/utils.js
@@ -1,2 +1,4 @@
 console.log((45 * 82).toString());
 console.log((Math.pow(15 / 2, 2) * Math.PI).toString());
+// Din fisierul utils.js, afiseaza in consola patratul sumei numerelor 13 si 41. a2  + 2ab + b2.
+console.log((Math.pow(13, 2) + 2 * 13 * 41 + Math.pow(41, 2)).toString());
