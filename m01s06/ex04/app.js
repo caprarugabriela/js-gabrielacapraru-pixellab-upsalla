aec6b78e89670b9121fe4949ebd530c829de8260
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2023-09-29 08:36:24 +0300

    Curs 28.09 - MS0106 - ex .03-06

--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -1,5 +1,29 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(function () {
+  const paragraphElement = document.createElement('p');
+  // adauga clasa message langa celelalte clase
+  paragraphElement.classList.add('message');
+  // in DOM, pe body, lipeste la sfarsitul doc - paragraf element
+  document.body.append(paragraphElement);
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', function () {
+      const message = 'Mouse-ul este pe scena';
+
+      console.log(message);
+      showMessage(message);
+    });
+
+    stage.addEventListener('mouseout', function () {
+      const message = 'Mouse-ul nu este pe scena';
+
+      console.log(message);
+      showMessage(message);
+    });
   });
+
+  function showMessage(message) {
+    paragraphElement.innerText = message;
+  }
 })();
