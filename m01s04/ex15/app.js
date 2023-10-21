53fb4f392fd5b1a3df77e1067cc8b3de19011dfd
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2023-10-21 15:52:26 +0300

    Curs 10.10 - M01S04 ex.15 + M02S02

--- m01s04/ex15/app.js
+++ m01s04/ex15/app.js
@@ -1,4 +1,4 @@
-var person = {
+const person = {
   name: 'Gabriela',
   surname: 'Capraru',
   age: 24,
@@ -32,15 +32,38 @@ var person = {
   ],
 };
 
-for (var i = 0; i < person.friends.length; i++) {
-  var friend1 = person.friends[i];
-  for (var j = 0; j < person.friends.length; j++) {
-    var friend2 = person.friends[j];
-    if (i !== j) {
-      var ageDifference = Math.abs(friend1.age - friend2.age);
-      console.log(
-        `Intre ${friend1.name} si ${friend2.name} este o diferenta de ${ageDifference} ani.`,
-      );
+// // intre x si y este o diferenta de abc ani.
+
+// Rezolvare 1 - eu
+
+// for (var i = 0; i < person.friends.length; i++) {
+//   var friend1 = person.friends[i];
+//   for (var j = 0; j < person.friends.length; j++) {
+//     var friend2 = person.friends[j];
+//     if (i !== j) {
+//       var ageDifference = Math.abs(friend1.age - friend2.age);
+//       console.log(
+//         `Intre ${friend1.name} si ${friend2.name} este o diferenta de ${ageDifference} ani.`,
+//       );
+//     }
+//   }
+// }
+
+// Rezolvare 2 - clasa
+const length = person.friends.length;
+for (let i = 0; i < length; ++i) {
+  // note 1: outerfriend ramane acelasi; practic, aceasta variabila este globala pentru bucla interioara (al doilea for) si locala pentru primul for
+  const outerFriend = person.friends[i];
+
+  for (let j = 0; j < length; ++j) {
+    if (i === j) {
+      continue;
     }
+    // note 2: daca suntem pe aceiasi indexare (Larry=Larry), facem un early exit cu continue (nu are sens sa avem de ex. Intre Larry si Larry exista o dif de 0 ani.)
+
+    const innerFriend = person.friends[j];
+    const ageDifference = Math.abs(outerFriend.age - innerFriend.age);
+    const message = `Intre ${outerFriend.name} si ${innerFriend.name} este o diferenta de ${ageDifference} ani.`;
+    console.log(message);
   }
 }
--- m02s02/ex01/app.js
+++ m02s02/ex01/app.js
@@ -1,19 +1,147 @@
 const form = document.getElementById('personForm');
+const personDisplayClass = 'personDisplay';
+// selectorul va aduce practic fieldsetul cu detalii
+const existingFieldset = form.querySelector('fieldset:nth-child(2)');
 
-const person = {
-  name: 'Gabriela',
-  surname: 'Capraru',
-  age: 24,
-};
+existingFieldset.append(renderSkillInput());
 
+// hoisted
+// returns (input and button)
+function renderSkillInput() {
+  // fragmentul de DOM --> document fragment permite generarea unui fragment de document care nu are parinte/nu exista
+  // renuntam astfel la un div(element intermediar) pe care nu il vrem (de ex. liste, tabele)
+  const container = new DocumentFragment();
+  const skillInput = document.createElement('input');
+  skillInput.type = 'text';
+  // adaugam clasa addSkill
+  skillInput.classList.add('addSkill');
+  // adaugam placeholder
+  skillInput.placeholder = 'Adauga Skill';
+  // adaugam butonasul, cu titlul si il appenduim si pe el
+  const skillInputButton = document.createElement('button');
+  skillInputButton.title = 'Adauga Skill';
+  skillInputButton.innerText = ' + ';
+
+  // La fiecare apasare a butonului, daca in input exista text, afiseaza abilitatea noua intr-o lista neordonata.
+  skillInputButton.addEventListener('click', function (event) {
+    const button = event.currentTarget;
+    // DOM traversal - traversarea DOM-ului
+    const skillInput = button.previousElementSibling;
+    const skillValue = skillInput.value;
+    if (skillValue.trim().length < 1) {
+      return;
+    }
+
+    button.after(renderSkillsUl(skillValue));
+
+    // cream lista neordonata langa buton
+    // renderSkillUl(skillValue)
+  });
+
+  container.append(skillInput);
+  container.append(skillInputButton);
+
+  return container;
+}
+
+function renderSkillsUl(skillName) {
+  const className = 'skillsUl';
+  let skillsUl = document.querySelector('.' + className);
+
+  if (skillsUl === null) {
+    skillsUl = document.createElement('ul');
+    skillsUl.classList.add(className);
+  }
+
+  const skillLi = document.createElement('li');
+  skillLi.innerText = skillName;
+
+  // In dreptul fiecarui element din lista adauga un buton de stergere.
+
+  const deleteSkillButton = document.createElement('button');
+  deleteSkillButton.type = 'button';
+  deleteSkillButton.innerText = 'Sterge';
+  deleteSkillButton.title = 'Sterge Skill';
+  deleteSkillButton.classList.add('deleteSkillButton');
+
+  // In momentul in care se apasa butonul, sterge elementul.
+  // Exemplu bad practice:
+  // deteleSkillButton.addEventListener ('click', function() { alert('test')}); - multe probleme mai ales cand avem mai multe li-uri - de evitat;
+  // rezolvarea se va face dupa form reset prin metoda addEventListener pe FORM
+
+  skillLi.append(deleteSkillButton);
+
+  skillsUl.append(skillLi);
+
+  return skillsUl;
+}
+
+// pentru evenimentul de submit adauga un eventlistener care este functia de mai jos;
+// prin event.preventDefault vrem sa prevenim comportamentul normal
 form.addEventListener('submit', function (event) {
   event.preventDefault();
+  const person = {};
+  // currentTarget => pointer catre obiectul de DOM pe care s-a rulat addEventListener
+  const form = event.currentTarget;
 
-  // refactor
-  const personDisplay = renderPerson(person);
-  // refactor
+  person.name = form.name.value;
+  person.surname = form.surname.value;
+  person.age = form.age.value;
+
+  clearDisplay();
+  const personDisplay = render(person);
   form.after(personDisplay);
+
+  form.reset();
 });
+
+// apasare buton stergere -> sterge elementul
+// bubly - evenimentul se intampla pe buton, dupa urca prin fiecare element pana ajunge la document (sau comparatia cu turnul)
+form.addEventListener('click', function () {
+  // obtain button from DOM
+  // target este obiectul DE PE CARE A PLECAT EVENIMENTUL
+  const target = event.target;
+  // check if actual button (vedem daca este butonul corect)
+  // nodeName = numele nodului, se scrie cu caps
+  // traducere = daca target.nodeName nu este buton SAU target.classlist nu include deleteskillbutton
+  // lasam evenimentul sa plece mai departe
+  if (
+    target.nodeName !== 'BUTTON' ||
+    !target.classList.contains('deleteSkillButton')
+  ) {
+    // return early;
+    return;
+  }
+  // readability hack
+  const deleteSkillButton = target;
+
+  // DOM tranversal
+  // button.parentElement.remove()
+  deleteSkillButton.parentElement.remove();
+});
+
+// hoisted
+// deduplicam rezultatul din display(consola) si aveam 2 variante: queryselector + remove (easiest one)
+// sau cu nextElementSibling si cateva if-uri + remove pe clasa personDisplay (thougher one)
+function clearDisplay() {
+  const display = document.querySelector('.' + personDisplayClass);
+
+  if (display !== null) {
+    display.remove();
+  }
+}
+
+function render(person) {
+  // in memory
+  const personDisplay = document.createElement('div');
+  personDisplay.classList.add(personDisplayClass);
+
+  personDisplay.append(renderPerson(person));
+
+  return personDisplay;
+}
+
+// hoisting at the top - a ridica, a urni (punem functia imediat sub locul in care va fi folosita)
 
 function renderPerson(person) {
   const paragraph = document.createElement('p');
