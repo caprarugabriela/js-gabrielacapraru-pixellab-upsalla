554ecd645fa0b3d60d3176d3ae3afd7d7e388c56
caprarugabriela <121718275+caprarugabriela@users.noreply.github.com>
2023-11-29 15:34:18 +0200

    Curs 14.11 - spread&destructure

--- spread-destructure/ex03/app.js
+++ spread-destructure/ex03/app.js
@@ -20,3 +20,26 @@ let age1 = {
 let age2 = {
   age: 25,
 };
+
+console.warn(`In fisierul app.js Andra are trebui sa aiba 25 de ani,
+Steven 20, iar Rolf 40, folosind operatorul spread, creeaza obiecte de tipul fullPerson1,
+fullPerson2 si fullPerson3 care sa reflecte acest lucru`);
+
+const fullPerson1 = { ...person1, ...age1 };
+
+console.log(fullPerson1);
+
+const fullPerson2 = {
+  age: 40,
+  ...person2,
+};
+console.log(fullPerson2);
+
+const fullPerson3 = {
+  ...person3,
+  ...age2,
+};
+console.log(fullPerson3);
+
+console.warn(`Folosind noul obiect fullPerson3 creeaza intr-un obiect numit
+fullPerson4 cu numele de familie al Andrei care sa se numeasca Anita si sa aiba 36 de ani.`);
